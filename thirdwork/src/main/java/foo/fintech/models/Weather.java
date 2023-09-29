db460896156027f262701333f0630cb78bde779f
Dazz1ee <ikatin.2003.sokol@gmail.com>
2023-09-29 22:52:33 +0300

    вроде бы убрал лишнее

--- thirdwork/src/main/java/foo/fintech/models/Weather.java
+++ thirdwork/src/main/java/foo/fintech/models/Weather.java
@@ -2,9 +2,7 @@ package foo.fintech.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Value;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @AllArgsConstructor
 @Data
