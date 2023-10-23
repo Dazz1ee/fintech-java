9e21ab4f2efd50511374e1eecc43fd29582ff1cb
Dazz1ee <ikatin.2003.sokol@gmail.com>
2023-10-23 15:15:23 +0300

    поменял на ConfigurationProperties

--- fintech/src/main/java/foo/configurations/UriBuilderConfig.java
+++ fintech/src/main/java/foo/configurations/UriBuilderConfig.java
@@ -1,22 +1,20 @@
 package foo.configurations;
 
 import foo.other.CustomUriBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "server")
 public class UriBuilderConfig {
-    @Value("${server.port}")
     public void setPort(Integer port) {
         CustomUriBuilder.setPort(port);
     }
-
-    @Value("${host}")
+    
     public void setHost(String host) {
         CustomUriBuilder.setHost(host);
     }
 
-    @Value("${schema}")
     public void setSchema(String schema) {
         CustomUriBuilder.setScheme(schema);
     }
